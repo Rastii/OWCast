f4d58b914ec12ae5fb40eab6f48b03145f7f113a
rastii <rastii09@gmail.com>
2016-07-01 01:12:50 -0400

    Added counterGenerator service scaffold for ricky

---
+++ src/app/components/counterGenerator/counterGenerator.service.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('owcast')
+        .service('counterGenerator', counterGenerator);
+
+    function counterGenerator() {
+        this.getCounterPlayers = function() {
+            // This is the scaffoled method i guess?
+        }
+    }
+})();
