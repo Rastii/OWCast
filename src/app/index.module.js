640558b793fa27bcb1bff082fe9d127247292ce1
rastii <rastii09@gmail.com>
2016-06-30 21:03:52 -0400

    Initial commit of scaffold.  Note: this includes all the asset files and default files generated by the gulp-angular yo generator

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('owcast', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
