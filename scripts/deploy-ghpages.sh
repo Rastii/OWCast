e4d1cddf0a869483596b65911841ef393171fae3
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-07-07 23:33:23 -0400

    Add travis files

---
+++ scripts/deploy-ghpages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf dist
+mkdir dist
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build
+bower install
+gulp
+
+# deploy
+cd dist
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
